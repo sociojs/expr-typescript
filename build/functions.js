ac37f0e29e78f811b42d3618e2b29e08cd5dcce8
T-Bee <gojavapro@gmail.com>
2022-06-06 13:42:52 +0000

    main

---
+++ build/functions.js
@@ -0,0 +1,5 @@
+"use strict";
+var checkWeather = function (city) {
+    return "Cloudy";
+};
+console.log(checkWeather('Jakarta'));
---
+++ src/functions.ts
@@ -0,0 +1,4 @@
+let checkWeather = (city: string): string => {
+    return "Cloudy"
+}
+console.log(checkWeather('Jakarta'))
